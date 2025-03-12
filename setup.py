425350d8da3b4f7f1449ccfa8998eb143faddd66
aakash-dec7 <Aakash.dec7@gmail.com>
2025-03-12 07:22:45 +0530

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="seq2seq",
+    version="1.0.0",
+    author="Aakash Singh",
+    author_email="aakash.dec7@gmail.com",
+    packages=find_packages(),
+)
